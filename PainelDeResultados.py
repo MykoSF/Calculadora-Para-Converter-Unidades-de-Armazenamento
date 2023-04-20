fa8ca74d87642cdea8096b7e6a15bba7468d852a
Maykon <mykosistema@gmail.com>
2023-04-20 13:47:39 -0300

    Este commit será objetificado para a manutenção do commit e código anterior e também adição de escolhas de função que usam como fonte a base importada do arquivo de cálculo 'BaseDeCalculos' para funcionar como painel de amostra de resultados para contas de conversão de unidades de armazenamento, com a possibilidade de escolha em relação à quais unidades você quer converter.

--- PainelDeResultados.py
+++ PainelDeResultados.py
@@ -4,3 +4,78 @@ print(' -Type 1 to convert bit to byte \n -Type 2 to convert Byte to Kilobyte \n
 choicefunc = input()
 
 
+if (choicefunc == '1'):
+    print ('you chose the option 1')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = BitToByte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '2'):
+    print ('you chose the option 2')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = ByteToKilobyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '3'):
+    print ('you chose the option 3')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = KilobyteToMegabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '4'):
+    print ('you chose the option 4')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = MegabyteToGigabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '5'):
+    print ('you chose the option 5')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = GigabyteToTerabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '6'):
+    print ('you chose the option 6')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = TerabyteToPetabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+
+elif (choicefunc == '7'):
+    print ('you chose the option 7')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = PetabyteToTerabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '8'):
+    print ('you chose the option 8')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = TerabyteToGigabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '9'):
+    print ('you chose the option 9')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = GigabyteToMegabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '10'):
+    print ('you chose the option 10')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = MegabyteToKilobyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '11'):
+    print ('you chose the option 11')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = KilobyteToByte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif (choicefunc == '12'):
+    print ('you chose the option 12')
+    entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+    valorConvertido = ByteToBit(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+else :
+    print ('Invalid')
