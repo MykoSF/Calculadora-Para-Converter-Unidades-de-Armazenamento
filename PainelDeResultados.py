fcb6d4bc21a87fa867df95cdc8f85038e8ff1927
Maykon <mykosistema@gmail.com>
2023-04-20 14:10:36 -0300

    Este commit é objetificado à remoção de dois arquivos indesejados vindos do cache temporário da troca de branches, O Painel de Amostra e a Base dos Cálculos.

--- PainelDeResultados.py
+++
@@ -1,6 +0,0 @@
-from BaseDeCalculos import converterStringParaFloat, BitToByte, ByteToKilobyte, KilobyteToMegabyte, MegabyteToGigabyte, GigabyteToTerabyte, TerabyteToPetabyte, PetabyteToTerabyte, TerabyteToGigabyte, GigabyteToMegabyte, MegabyteToKilobyte, KilobyteToByte, ByteToBit
-
-print(' -Type 1 to convert bit to byte \n -Type 2 to convert Byte to Kilobyte \n -Type 3 to convert Kilobyte to Megabyte \n -Type 4 to convert Megabyte to Gigabyte \n -Type 5 to convert Gigabyte to Terabyte \n -Type 6 to convert Terabyte to Petabyte \n -Type 7 to convert Petabyte to Terabyte \n -Type 8 to convert Terabyte to Gigabyte \n -Type 9 to convert Gigabyte to Megabyte \n -Type 10 to convert Megabyte to Kilobyte \n -Type 11 to convert Kilobyte to Byte \n -Type 12 to convert Byte to Bit')
-choicefunc = input()
-
-
