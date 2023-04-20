8aad0adfb4d08898d7515d5102b6b9162e3e6304
Maykon <mykosistema@gmail.com>
2023-04-20 16:36:11 -0300

    Este commit será objetificado para a manutenção do código com o objetivo de conversão das unidades de armazenamento.

--- CalculadoraV2_Estrutura_Loop.py
+++ CalculadoraV2_Estrutura_Loop.py
@@ -3,26 +3,35 @@ unidades = ["bit", "byte", "kilobyte", "megabyte", "gigabyte", "terabyte", "peta
 def converter_medidas(valor, unidade_entrada, unidade_saida):
     indice_origem = unidades.index(unidade_entrada)
     indice_destino = unidades.index(unidade_saida)
-    fator = 1024**(indice_destino - indice_origem)
-    return valor * fator
+    if unidade_entrada == "bit":
+        fator = 1
+        fator *= (indice_destino - indice_origem) * 8
+    else:
+        fator = 1024
+        fator **= (indice_destino - indice_origem)
 
-quantidade = int(input("Digite a quantidade de armazenamento em números naturais a ser convertida: "))
+    if indice_origem < indice_destino:
+        return (valor / fator)
+    else:
+        return (valor * fator)
+
+while True:
+    try:
+        quantidade = int(input("Digite a quantidade de armazenamento em números naturais a ser convertida: "))
+        break
+    except ValueError:
+        print("Quantidade inválida. Por favor, digite um número inteiro.")
 
 unidade_entrada = input("Digite a unidade a ser convertida: ")
-
-while unidade_entrada not in unidades :
+while unidade_entrada not in unidades:
     print("Unidade não reconhecida, digite a unidade de conversão correta: ")
     unidade_entrada = input()
 
 unidade_saida = input("Digite a unidade para converter em: ")
-
-while unidade_saida not in unidades :
+while unidade_saida not in unidades:
     print("Unidade não reconhecida, digite a unidade de conversão correta: ")
     unidade_saida = input()
-    
-while unidades.index(unidade_entrada) < unidades.index(unidade_saida):
-    nova_unidade = unidades[unidades.index(unidade_entrada) + 1]
-    quantidade = converter_medidas(quantidade, unidade_entrada, nova_unidade)
-    unidade_origem = nova_unidade
 
-print("O resultado da conversão é:", quantidade, unidade_entrada)
+quantidade = converter_medidas(quantidade, unidade_entrada, unidade_saida)
+
+print("O resultado da conversão é:", quantidade, unidade_saida)
