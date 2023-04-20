de13a9319a1100e2e63e183fbeec6062bb1c096e
Maykon <mykosistema@gmail.com>
2023-04-20 10:29:52 -0300

    Este commit será objetificado para a adição da base de cálculos à branch atual, para ser utilizado como fonte de importação para o painel de resultados do atual programa, a conversão das unidades de armazenamento: bit, byte, kilobyte, megabyte, gigabyte, terabyte e também petabyte.

---
+++ BaseDeCalculos.py
@@ -0,0 +1,76 @@
+def converterStringParaFloat(value):
+    print('Valor convertido de str para float')
+    return float(value)
+
+
+def BitToByte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    bytesCalculadoDivisão = valorASerConvertido / 8
+    return bytesCalculadoDivisão
+
+
+def ByteToKilobyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    KilobyteCalculadoDivisão = valorASerConvertido / 1024
+    return KilobyteCalculadoDivisão
+
+
+def KilobyteToMegabyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    MegabyteCalculadoDivisão = valorASerConvertido / 1024
+    return MegabyteCalculadoDivisão
+
+
+def MegabyteToGigabyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    GigabyteCalculadoDivisão = valorASerConvertido / 1024
+    return GigabyteCalculadoDivisão
+
+
+def GigabyteToTerabyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    TerabyteCalculadoDivisão = valorASerConvertido / 1024
+    return TerabyteCalculadoDivisão
+
+
+def TerabyteToPetabyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    PetabyteCalculadoDivisão = valorASerConvertido / 1024
+    return PetabyteCalculadoDivisão
+
+
+def PetabyteToTerabyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    TerabyteCalculadoMultiplicar = valorASerConvertido * 1024
+    return TerabyteCalculadoMultiplicar
+
+
+def TerabyteToGigabyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    GigabyteCalculadoMultiplicar = valorASerConvertido * 1024
+    return GigabyteCalculadoMultiplicar
+
+
+def GigabyteToMegabyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    MegabyteCalculadoMultiplicar = valorASerConvertido * 1024
+    return MegabyteCalculadoMultiplicar
+
+
+def MegabyteToKilobyte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    KilobyteCalculadoMultiplicar = valorASerConvertido * 1024
+    return KilobyteCalculadoMultiplicar
+
+
+def KilobyteToByte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    KilobyteCalculadoMultiplicar = valorASerConvertido * 1024
+    return KilobyteCalculadoMultiplicar
+
+
+def ByteToBit(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    BitCalculadoMultiplicar = valorASerConvertido * 8
+    return BitCalculadoMultiplicar
+
