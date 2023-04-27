81cc78ef97ed658df181f431c6895abf3f3cd5d0
Maykon <mykosistema@gmail.com>
2023-04-27 15:24:36 -0300

    Este commit é objetificado à manutenção e finalização da calculadora de conversão de unidades de armazenamento utilizando todos os meio aprendidos no torno de 8 dias de aula, ela será funcional através da utilização de estruturas de repetição e listas dentro da linguagem Python.

--- CalculadoraV2_Estrutura_Loop.py
+++ CalculadoraV2_Estrutura_Loop.py
@@ -1,20 +1,5 @@
 unidades = ["bit", "byte", "kilobyte", "megabyte", "gigabyte", "terabyte", "petabyte"]
 
-def converter_medidas(valor, unidade_entrada, unidade_saida):
-    indice_origem = unidades.index(unidade_entrada)
-    indice_destino = unidades.index(unidade_saida)
-    if unidade_entrada == "bit":
-        fator = 1
-        fator *= (indice_destino - indice_origem) * 8
-    else:
-        fator = 1024
-        fator **= (indice_destino - indice_origem)
-
-    if indice_origem < indice_destino:
-        return (valor / fator)
-    else:
-        return (valor * fator)
-
 while True:
     try:
         quantidade = int(input("Digite a quantidade de armazenamento em números naturais a ser convertida: "))
@@ -31,6 +16,21 @@ unidade_saida = input("Digite a unidade para converter em: ")
 while unidade_saida not in unidades:
     print("Unidade não reconhecida, digite a unidade de conversão correta: ")
     unidade_saida = input()
+
+def converter_medidas(valor, unidade_entrada, unidade_saida):
+    indice_origem = unidades.index(unidade_entrada)
+    indice_destino = unidades.index(unidade_saida)
+
+    if unidade_entrada == 'bit':
+        valor /= 8
+        unidade_entrada = 'byte'
+    
+    if unidade_saida == 'bit':
+        valor /= 8
+        unidade_saida = 'byte'
+    
+    fator = 1024 ** (indice_destino - indice_origem)
+    return valor / fator
 
 quantidade = converter_medidas(quantidade, unidade_entrada, unidade_saida)
 
